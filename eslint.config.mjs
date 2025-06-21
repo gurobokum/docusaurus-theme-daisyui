96f1dde501773a26f92c97e3c8b4b0bd4c9609d0
Guro Bokum <jiojiajiu@gmail.com>
2025-06-21 15:49:52 +0300

    build: add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -14,4 +17,5 @@ export default defineConfig([
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   tseslint.configs.recommended,
+  storybook.configs["flat/recommended"],
 ]);
