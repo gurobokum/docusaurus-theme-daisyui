c8a6b5101d06cbb678051cc6640a3e44ef4de581
Guro Bokum <jiojiajiu@gmail.com>
2025-07-08 23:46:59 +0300

    feat: add TailwindCSS and DaisyUI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-//import type { Plugin } from "@docusaurus/types";
-//export { validateOptions, validateThemeConfig } from "@docusaurus/theme-common";
+import path from "path";
+import { Joi } from "@docusaurus/utils-validation";
+import type {
+  ThemeConfigValidationContext,
+  OptionValidationContext,
+  PostCssOptions,
+} from "@docusaurus/types";
 
+// Docs: https://docusaurus.io/docs/api/plugin-methods
 export default function themeDaisyui() {
   return {
     name: "docusaurus-theme-daisyui",
@@ -8,10 +14,43 @@ export default function themeDaisyui() {
     getThemePath() {
       return "../lib/theme";
     },
+
     getTypeScriptThemePath() {
       return "../src/theme";
+    },
+
+    configurePostCss(postcssOptions: PostCssOptions) {
+      postcssOptions.plugins = [
+        ...(postcssOptions.plugins || []),
+        "@tailwindcss/postcss",
+      ];
+      return postcssOptions;
+    },
+
+    getClientModules() {
+      return [path.resolve(__dirname, "./global.css")];
     },
   };
 }
 
-//export function validateOptions({ options, validate }) {}
+export type ThemeConfig = object;
+export type PluginOptions = object;
+export type Options = PluginOptions;
+
+export const Schema = Joi.object<ThemeConfig>({});
+export const PluginOptionSchema = Joi.object<PluginOptions>({});
+
+export function validateThemeConfig({
+  validate,
+  themeConfig,
+}: ThemeConfigValidationContext<ThemeConfig>): ThemeConfig {
+  return validate(Schema, themeConfig);
+}
+
+export function validateOptions({
+  validate,
+  options,
+}: OptionValidationContext<Options, PluginOptions>): PluginOptions {
+  const validatedOptions = validate(PluginOptionSchema, options);
+  return validatedOptions;
+}
